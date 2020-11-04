1d777ccc0f58ee73c90d93e3df69dfdd52899236
Mafrans <malte.kluft@gmail.com>
2020-11-04 19:44:49 +0100

    Add history logic

--- src/me/mafrans/loggbok/History.kt
+++ src/me/mafrans/loggbok/History.kt
@@ -4,6 +4,12 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class History {
+    companion object {
+        fun makeID(): String {
+            return String.format("#%06x", Random().nextInt(0xffffff + 1))
+        }
+    }
+
     val entries: List<HistoryEntry> = ArrayList();
 
     fun getBetween(fromIndex: Int, toIndex: Int): List<HistoryEntry> {
@@ -30,10 +36,24 @@ abstract class HistoryEntry(val author: Author) {
 
 class HistoryPageCreatedEntry(author: Author, page: Page) : HistoryEntry(author) {
     val page: Page = page
+    val id: String = History.makeID()
 
     override fun getMessage(): String {
         if(page.volume != null) {
             return "$author created page ${page.index} in ${page.volume.book.title}, Volume ${page.volume.index}"
+        }
+
+        return "$author created page ${page.index}"
+    }
+}
+
+class HistoryPageEditedEntry(author: Author, page: Page, changeState: TextChangeState) : HistoryEntry(author) {
+    val id: String = History.makeID()
+    val page: Page = page
+
+    override fun getMessage(): String {
+        if(page.volume != null) {
+            return "$author edited page ${page.index} in ${page.volume.book.title}, Volume ${page.volume.index}"
         }
 
         return "$author created page ${page.index}"
